31127e0487c1d8254a28b7d110c6ef19a7e09e57
pknowledge <abhishekrawane1195@gmail.com>
2021-11-02 00:15:08 +0530

    added: Quotient & Remainder program with analysis

---
+++ src/com/bridgelabz/QuotientRemainder.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class QuotientRemainder {
+
+	public static void main(String[] args)
+	{
+		int Divident=5;
+		int Divisor=2;
+		int Quotient=Divident/Divisor;
+		int Remainder=Divident%Divisor;
+		
+		System.out.println("Quotient = "+ Quotient);
+		System.out.println("Remainder = "+ Remainder);
+
+		
+	}
+
+}
